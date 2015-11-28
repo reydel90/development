94624f09da21bb126243de63cec5fd62ca476cd7
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-11-29 00:27:29 +0100

    added navbar

--- public_html/templates/default/blocks/head.php
+++ public_html/templates/default/blocks/head.php
@@ -1,9 +1,3 @@
-<?php
-header ( "Cache-Control: must-revalidate");
-$offset = 60 * 60 * 24 * 30; /*Aquí se especifica los dias*/
-$ExpireString = "Expires:" . gmdate("D, d M Y H: i: s", time () + $offset) . "GMT";
-header ($ExpireString);
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -11,3 +5,4 @@ header ($ExpireString);
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<title><?php if(!empty($data['title'])){echo $data['title'];}else{echo 'Default title';}?></title>
 </head>
+<body>
--- public_html/templates/default/start.php
+++ public_html/templates/default/start.php
@@ -1,4 +1,4 @@
 <?php
 	include_once Controller::template() . 'blocks' . DS . 'head.php';
-	include_once Controller::template() . 'blocks' . DS . 'nav.php';
+	include_once Controller::template() . 'blocks' . DS . 'navbar.php';
 	
