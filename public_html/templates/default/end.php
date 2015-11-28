701d13276912a52312850876713520afa15b6e8f
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-11-29 00:34:35 +0100

    added bootstrap and navbar

--- public_html/templates/default/end.php
+++ public_html/templates/default/end.php
@@ -1,3 +1,4 @@
 </body>
 </html>
-    <link href="<?php echo Controller::template_assets() . 'css/custom.css';?>" rel="stylesheet">          	
+    <link href="<?php echo Controller::template_assets() . 'css/custom.css';?>" rel="stylesheet">      
+    <link href="<?php echo Controller::template_assets() . 'css/bootstrap.min.css';?>" rel="stylesheet">         	
