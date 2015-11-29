42eb0a8a4129b33ad701370cf8e48424db1d5b11
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-11-29 01:10:52 +0100

    test 3

--- public_html/templates/default/blocks/head.php
+++ public_html/templates/default/blocks/head.php
@@ -4,5 +4,7 @@
 	<meta charset="utf-8">           
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"> 
 	<title><?php if(!empty($data['title'])){echo $data['title'];}else{echo 'Default title';}?></title>
+    <link href="<?php echo Controller::template_assets() . 'css/custom.css';?>" rel="stylesheet">      
+    <link href="<?php echo Controller::template_assets() . 'css/bootstrap.css';?>" rel="stylesheet"> 	
 </head>
 <body>
