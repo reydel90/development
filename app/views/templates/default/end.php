6b54063bdd22aab6198436b4e265056d83349225
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-11-29 21:05:55 +0100

    test 21

--- app/views/templates/default/end.php
+++ app/views/templates/default/end.php
@@ -1,3 +1,4 @@
 </body>
 </html>
 <link rel="stylesheet" type="text/css" href="<?php echo Controller::assets('css','bootstrap'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo Controller::assets('css', 'custom'); ?>">
