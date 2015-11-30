89c92fa041abfc6844c3473257408940d0ea89fe
reydel90 <reydel25@icloud.com>
2015-11-30 19:38:02 +0100

    test detect 4

--- app/controllers/pageController.php
+++ app/controllers/pageController.php
@@ -3,8 +3,7 @@ class pageController extends Controller{
 	protected $title  = '';	  
 
 	public function index(){
-		echo Controller::detect();
 		$this->title = 'home';
-		$this->view('page/index', ['title' => $this->title]);
+		$this->view('page/index', ['title' => $this->title, 'device' = $this->detect()]);
 	}
 }
