3ecc054a671a09ed16307109e6c61b16234f2a64
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-12-14 18:47:29 +0100

    test request 2

--- app/controllers/pageController.php
+++ app/controllers/pageController.php
@@ -1,15 +1,10 @@
 <?php
 class pageController extends Controller{
-	protected $title  = '',
-			  $device = $this->detect();	  
+	protected $title  = '';	  
 
 	public function index(){
 		$this->title = 'home';
-		//$device = $this->detect();
-		$this->view('page/index', ['title' => $this->title, 'device' => $this->$device]);		
-	}
-	public function contact(){
-		$this->title = 'Contact';
-		$this->view('contact', ['title' => $this->title, 'device' => $this->$device]);
+		$device = $this->detect();
+		$this->view('page/index', ['title' => $this->title, 'device' => $device]);		
 	}
 }
