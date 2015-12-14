07b865c6ec9ede3dbf4fd11af245c2209d0457e5
Laia Galit� Vall-llaura <Laia Galit Vall-llaura>
2015-12-15 00:44:05 +0100

    page speed

--- app/views/templates/default/main.php
+++ app/views/templates/default/main.php
@@ -8,11 +8,11 @@
 	    <meta name="apple-mobile-web-app-capable" content="yes">
 	    <meta name="apple-mobile-web-app-status-bar-style" content="black">
 		<title><?php if(!empty($data['title'])){echo $data['title'];}else{echo 'Default title';}?></title>
-		<link rel="stylesheet" type="text/css" href="<?php echo Controller::assets('css','application'); ?>">
 	</head>
 <body>
 	<h1>este es el template default</h1>
 	<?php Controller::content();?><!-- Funcion encargada de cargar la vista requerida -->
 	<h3>dispositivo usado : <?php echo $data['device'];?></h3>
+	<link rel="stylesheet" type="text/css" href="<?php echo Controller::assets('css','application'); ?>">
 </body>
 </html>
